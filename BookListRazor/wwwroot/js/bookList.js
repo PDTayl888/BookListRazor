b6f3db4c1771e0247304593bb1625b3bacb6cb12
pdtayl888 <patrickdtaylor1980@gmail.com>
2020-03-23 23:54:56 -0400

    added delete toastr

--- BookListRazor/wwwroot/js/bookList.js
+++ BookListRazor/wwwroot/js/bookList.js
@@ -39,3 +39,29 @@ function loadDataTable() {
         "width": "100%"
     });
 }
+
+function Delete(url) {
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover",
+        icon: "warning",
+        dangerMode: true 
+    }).then((willDelete) => {
+        if (willDelete) {
+            console.log(willDelete);
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
